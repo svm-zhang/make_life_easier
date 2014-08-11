da9f32d6c7aad17437e4532792c73a2e4ad39e35
Simo Zhang <zhangsimo850114@gmail.com>
2014-08-11 10:33:10 -0400

    fq2fa.py: add Fx2Fy base class, add Fqs2Fas and Fa2Fq class

--- fq2fa.py
+++ fq2fa.py
@@ -1,18 +1,38 @@
 import sys
+import os
+import multiprocessing
 
-class Fq2Fa:
-	def __init__(self, fq="", fa=""):
-		self.fa = fa
-		self.fq = fq
+import io_simo
+from ftsensor import FtSensor
+
+class Fx2Fy(object):
+	'''
+		from FileA to FileB
+	'''
+	def __init__(self, file_a, file_b):
+		self.file_a = file_a
+		self.file_b = os.path.realpath(file_b)
+		io_simo.check_file_if_exists(file_a)
+		io_simo.make_dir_if_necessary(os.path.dirname(self.file_b))
+
+class Fq2Fa(Fx2Fy):
+	''' from FastQ to FastA '''
+	def __init__(self, file_a, file_b):
+		super(Fq2Fa, self).__init__(file_a, file_b)
 
 	def start(self):
-		return self.__run()
+		# make sure the input file is FastQ
+		if FtSensor(self.file_a).getfiletype() != "fq":
+			sys.stderr.write("[FOMAT Error] %s is not in FastQ format\n" %(self.file_a))
+			sys.exit()
+		else:
+			sys.stdout.write("[Fq2Fa] converting %s to %s\n" %(self.file_a, self.file_b))
+			return self._run()
 
-	def __run(self):
-		print "parent run() function"
+	def _run(self):
 		num_seq = 0
-		fFA = open(self.fa, 'w')
-		with open(self.fq, 'r') as fFQ:
+		fFA = open(self.file_b, 'w')
+		with open(self.file_a, 'r') as fFQ:
 			for i, line in enumerate(fFQ):
 				if i % 4 == 0:
 					fFA.write(">" + line[1:])
@@ -23,35 +43,58 @@ class Fq2Fa:
 				fFA.flush()
 		return num_seq
 
-class FqSeq2Fa(Fq2Fa):
-	def __init__(self, fq_seq="", fa=""):
-		self.fq_seq = fq_seq
-		self.fa = fa
-		self.num_seq = 0
+class Fqs2Fas(Fq2Fa):
+	''' from FastQ to FastA mp mode '''
+	def __init__(self, files, nproc):
+		self.files = files
+		self.nproc = nproc
 
 	def start(self):
-		self.__run()
-		return self.num_seq
-
-	def __run(self):
+		task_q = multiprocessing.JoinableQueue()
+		for _ in range(self.nproc):
+			p = multiprocessing.Process(target=self._run, args=(task_q, ))
+			p.daemon = True
+			p.start()
 		i = 0
-		fFA = open(self.fa, 'w')
-		for line in self.fq_seq.splitlines():
-			if i % 4 == 0:
-				fFA.write(">" + line[1:] + "\n")
-				i += 1
-			elif i % 4 == 1:
-				fFA.write(line + "\n")
-				i += 1
-			elif i % 4 == 2:
-				i += 1
-			else:
-				self.num_seq += 1
-				i = 0
-			fFA.flush()
-		fFA.close()
-		return
+		while i < len(self.files):
+			fq = os.path.realpath(self.files[i])
+			fa = os.path.realpath(self.files[i+1])
+			io_simo.check_file_if_exists(fq)
+			io_simo.make_dir_if_necessary(os.path.dirname(fa))
+			if FtSensor(fq).getfiletype() != "fq":
+				sys.stderr.write("[FOMAT Error] %s is not in FastQ format\n" %(fq))
+				sys.exit()
+			task_q.put((fq, fa))
+			i += 2
+		try:
+			task_q.join()
+		except KeyboardInterrupt:
+			sys.stderr.write("Terminated unexpectedly by keyboard\n")
+			sys.exit()
 
-#fq = sys.argv[1]
-#fa = sys.argv[2]
-#Fq2Fa(fq, fa).start()
+	def _run(self, task_q):
+		while True:
+			try:
+				fq, fa = task_q.get()
+				sys.stdout.write("%s\tconverting %s\t%s\n" %(multiprocessing.current_process().name, fq, fa))
+				num_seq = 0
+				fFA = open(fa, 'w')
+				with open(fq, 'r') as fFQ:
+					for i, line in enumerate(fFQ):
+						if i % 4 == 0:
+							fFA.write(">" + line[1:])
+						elif i % 4 == 1:
+							fFA.write(line)
+						else:
+							num_seq += 1
+						fFA.flush()
+			finally:
+				task_q.task_done()
+
+class Fa2Fq(Fx2Fy):
+	'''
+		from FastA to FastQ
+	'''
+	def __init__(self, file_a, file_b, qual_file = ""):
+		super(Fa2Fq, self).__init__(file_a, file_b)
+		self.qual_file = qual_file
