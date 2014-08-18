1d8e11d212145cdd62793b9dfdeddf908263fdd6
Simo Zhang <zhangsimo850114@gmail.com>
2014-08-17 23:29:17 -0400

    minor changes

--- io_simo.py
+++ io_simo.py
@@ -2,8 +2,9 @@ import os
 import sys
 
 def check_file_if_exists(path):
-	if not os.path.exists(path):
-		sys.stdout.write("[IO Error] Cannot find file %s\n" %(path))
+	abs_path = os.path.realpath(path)
+	if not os.path.exists(abs_path):
+		sys.stdout.write("[IO Error] Cannot find file %s\n" %(abs_path))
 		sys.exit(1)
 
 def check_files_if_exist(*paths):
@@ -15,5 +16,5 @@ def make_dir_if_necessary(path):
 		os.makedirs(path)
 
 def make_dirs_if_necessary(*paths):
-	for dir in paths:
-		make_dir_if_necessary(dir)
+	for path in paths:
+		make_dir_if_necessary(path)
