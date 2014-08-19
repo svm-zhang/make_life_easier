dc0153723132b6c7c98bd688a36224817db33dc5
Simo Zhang <zhangsimo850114@gmail.com>
2014-08-18 22:29:45 -0400

    add FixPE_Save class

--- fixpe.py
+++ fixpe.py
@@ -4,6 +4,8 @@
 import re
 import os
 import sys
+import subprocess
+import shlex
 
 from io_simo import check_files_if_exist
 from io_simo import make_dir_if_necessary
@@ -32,23 +34,27 @@ class FixPE(object):
 				sys.exit(1)
 
 	def _get_splitby(self, file):
-		hfmt, _ = FtSensor(file).getencoding()
+		hfmt = FtSensor(file).gethfmt()
 		if hfmt == "illumina14":
 			return "/"
 		elif hfmt == "casava18":
 			return " "
+		elif hfmt == "mix":
+			sys.stderr.write("[FMT CHECKER] %s includes at least two different header formats\n")
+			sys.exit(1)
 
 	def start(self):
-		self._fix_pairing()
+		self._fixpe()
 
-	def _fix_pairing(self):
+	def _fixpe(self):
+		''' fix reads pairing '''
 		id, str_except_id = "", ""
 		fq1_dict = {}
-		split_by = self._get_splitby(self.fq1)
+		split_by1 = self._get_splitby(self.fq1)
 		with open(self.fq1, 'r') as fFQ1:
 			for i, line in enumerate(fFQ1):
 				if i % 4 == 0 :								# every four line represents one read #
-					tmp_line = line.split(split_by)
+					tmp_line = line.split(split_by1)
 					id = tmp_line[0]
 					str_except_id = tmp_line[1]
 #					if i % 10000 == 0 and i != 0:
@@ -59,94 +65,74 @@ class FixPE(object):
 					if i % 4 == 3:
 						fq1_dict[id] = str_except_id
 						str_except_id = ""
-		sys.stdout.write("\nBefore:\n")
-		sys.stdout.write("%s: %d reads\n" %(self.fq1, (i+1)/4))
-		sys.stdout.flush()
+		nreads1 = (i+1)/4
 
 		fOFQ1 = open(self.outp + "_r1.preqc.fastq", 'w')
 		fOFQ2 = open(self.outp + "_r2.preqc.fastq", 'w')
 		fORPHAN = open(self.outp + "_orphan.preqc.fastq", 'w')
 
-		split_by = self._get_splitby(self.fq1)
+		split_by2 = self._get_splitby(self.fq2)
 		num_paired, num_orphan = 0, 0
 		with open(self.fq2, 'r') as fFQ2:
 			for i, line in enumerate(fFQ2):
 				if i % 4 == 0:
-					tmp_line = line.split(split_by)
+					tmp_line = line.split(split_by2)
 					id = tmp_line[0]
 					str_except_id = tmp_line[1]
 				else:
 					str_except_id += line
 					if i % 4 == 3:
 						if id in fq1_dict:
-							fOFQ1.write(id + " %s" %(fq1_dict[id]))
-							fOFQ2.write(id + " %s" %(str_except_id))
+							fOFQ1.write(id + "%s%s" %(split_by1, fq1_dict[id]))
+							fOFQ2.write(id + "%s%s" %(split_by2, str_except_id))
 							num_paired += 1
 							fq1_dict.pop(id)
 						else:
-							fORPHAN.write(id + " %s" %(str_except_id))
+							fORPHAN.write(id + "%s%s" %(split_by2, str_except_id))
 							num_orphan += 1
 						str_except_id = ""
-		sys.stdout.write("%s: %d reads\n" %(self.fq2, (i+1)/4))
 		for id, str_except_id in fq1_dict.iteritems():
-			fORPHAN.write(id + " %s" %(str_except_id))
+			fORPHAN.write(id + "%s%s" %(split_by1, str_except_id))
 			num_orphan += 1
 		fOFQ1.close()
 		fOFQ2.close()
 		fORPHAN.close()
 
+		sys.stdout.write("\nBefore:\n")
+		sys.stdout.write("%s: %d reads\n" %(self.fq1, nreads1))
+		sys.stdout.write("%s: %d reads\n" %(self.fq2, (i+1)/4))
 		sys.stdout.write("After:\n")
 		sys.stdout.write("%s: %d\n" %(self.outp + "_r1.preqc.fastq", num_paired))
 		sys.stdout.write("%s: %d\n" %(self.outp + "_r2.preqc.fastq", num_paired))
 		sys.stdout.write("%s: %d\n\n" %(self.outp + "_orphan.preqc.fastq", num_orphan))
+		sys.stdout.flush()
 
 class FixPE_Save(FixPE):
 	def __init__(self, args):
-		super(FixPE_Save, self).__init(args)
+		sys.stdout.write("[FIXPE] Using the memory saving mode\n")
+		super(FixPE_Save, self).__init__(args)
 
-	def get_header_pattern(fq):
-		cmd = "head -n1 %s" %(fq1)
-		p = subprocess.Popen(shlex.split(cmd), stdout = subprocess.PIPE)
-		return p.communicate()[0].strip().split(':')[0]
+	def _get_indices(self, file, index):
+		if os.path.exists(index):
+			sys.stdout.write("skip indexing %s\n" %(index))
+			return
+		sys.stdout.write("get indices: %s\n" %(file))
+		hfmt = FtSensor(file).gethfmt()
+#		grep_cmd = "grep -n ^%s %s" %(header_pattern, fq)
+#		awk_cmd = "awk '{split($0, a, \":\"); b = substr($0, index($0, \":\")+1); print b,a[1]}' "
+#		pgrep = subprocess.Popen(shlex.split(grep_cmd), stdout = subprocess.PIPE)
+#		sort_cmd = "sort -n -k2 "
+		awk_cmd = "awk \'{if(NR %% 4 == 1) {split($0, a, \"/\"); print NR,a[1];}}\' %s" %(file)
+		pawk = subprocess.Popen(shlex.split(awk_cmd), stdout = subprocess.PIPE)
+#		psort = subprocess.Popen(shlex.split(sort_cmd), stdin = pawk.stdout, stdout = subprocess.PIPE)
+#		psort_out = psort.communicate()[0]
+		pawk_out = pawk.communicate()[0]
+		fINDEX = open(index, 'w')
+		fINDEX.write(pawk_out)
+		fINDEX.close()
+		return
 
-	def get_ids(basename, fq, header_pattern):
-		id_file = basename + ".ids"
-		grep_cmd = "grep ^%s %s" %(header_pattern, fq)
-		awk_cmd = "awk '{print $1}' "
-		sort_cmd = "sort "
-		pgrep = subprocess.Popen(shlex.split(grep_cmd), stdout = subprocess.PIPE)
-		pawk = subprocess.Popen(shlex.split(awk_cmd), stdin = pgrep.stdout, stdout = subprocess.PIPE)
-		psort = subprocess.Popen(shlex.split(sort_cmd), stdin = pawk.stdout, stdout = open(id_file, 'w'))
-		psort.wait()
-		return id_file
-
-	def get_union_ids(id_file_1, id_file_2):
-		all_id_file = "all.ids"
-		cat_cmd = "cat %s %s" %(id_file_1, id_file_2)
-		sort_cmd = "sort "
-		uniq_cmd = "uniq "
-		pcat = subprocess.Popen(shlex.split(cat_cmd), stdout = subprocess.PIPE)
-		psort = subprocess.Popen(shlex.split(sort_cmd), stdin = pcat.stdout, stdout = subprocess.PIPE)
-		puniq = subprocess.Popen(shlex.split(uniq_cmd), stdin = psort.stdout, stdout = open(all_id_file, 'w'))
-		puniq.wait()
-		return all_id_file
-
-	def get_indices(basename, fq, header_pattern):
-		index_file = basename + ".index"
-		if os.path.exists(index_file):
-			print "skip indexing %s" %(index_file)
-			return index_file
-		print "get indices: %s" %(fq)
-		grep_cmd = "grep -n ^%s %s" %(header_pattern, fq)
-		awk_cmd = "awk '{split($0, a, \":\"); b = substr($0, index($0, \":\")+1); print b,a[1]}' "
-		sort_cmd = "sort -n -k3 "
-		pgrep = subprocess.Popen(shlex.split(grep_cmd), stdout = subprocess.PIPE)
-		pawk = subprocess.Popen(shlex.split(awk_cmd), stdin = pgrep.stdout, stdout = subprocess.PIPE)
-		psort = subprocess.Popen(shlex.split(sort_cmd), stdin = pawk.stdout, stdout = open(index_file, 'w'))
-		psort.wait()
-		return index_file
-
-	def get_seq(pdict, fq, fP, fSE):
+	def _get_seq(self, pdict, fq, fP, fORPHAN):
 		missing = -1
 		for line_num, line in enumerate(open(fq, 'r')):
 			if line_num % 4 == 0:
@@ -154,32 +140,42 @@ class FixPE_Save(FixPE):
 					if pdict[line_num+1] == 1:
 						fP.write(line)
 						missing = 0
+						del pdict[line_num+1]
 					else:
-						fSE.write(line)
+						fORPHAN.write(line)
 						missing = 1
 			else:
 				if not missing:
 					fP.write(line)
 				else:
-					fSE.write(line)
+					fORPHAN.write(line)
 			fP.flush()
-			fSE.flush()
+			fORPHAN.flush()
+#		if os.getpid() > 0:
+#			os._exit(0)
 
-	def fix_pairing(index_file_1, index_file_2, fq1, fq2, paired_file_1, paired_file_2, se_file):
-		print "fixing pairing"
+	def start(self):
+		self._fixpe_save()
+
+	def _fixpe_save(self):
+		''' fix reads pairng in memmory saving mode '''
+		index1 = self.outp + "_r1.index"
+		index2 = self.outp + "_r2.index"
+		self._get_indices(self.fq1, index1)
+		self._get_indices(self.fq2, index2)
+
+		sys.stdout.write("[FIXPE] Matching Reads ID ...\n")
 		awk_cmd = """
-				awk 'NR==FNR {a[$1] = $3; next;}
-				{if($1 in a)
-					{print $1, a[$1], $3; delete a[$1];}
+				awk 'NR==FNR {a[$2] = $1; next;}
+				{if($2 in a)
+					{print $2, a[$2], $1; delete a[$2];}
 				 else
-					{print $1, \"-2\", $3;}}
+					{print $2, \"-2\", $1;}}
 				END {for (i in a) {print i, \"-1\", a[i];}}' %s %s
-				""" %(index_file_1, index_file_2)
+				""" %(index1, index2)
 		pawk = subprocess.Popen(shlex.split(awk_cmd.strip()), stdout = subprocess.PIPE)
-		fPAIR1 = open(paired_file_1, 'w')
-		fPAIR2 = open(paired_file_2, 'w')
-		fSE = open(se_file, 'w')
 
+		sys.stdout.write("[FIXPE] Making pairs and rescuing orphans ...\n")
 		pdict1, pdict2 = {}, {}
 		sedict = {}
 		for line in pawk.communicate()[0].splitlines():
@@ -189,28 +185,39 @@ class FixPE_Save(FixPE):
 				pdict2[int(tmp_line[2])] = 1
 			else:
 				if int(tmp_line[1]) == -1:
-					pdict1[int(tmp_line[2])] = int(tmp_line[1])
+					pdict1[int(tmp_line[2])] = -1
 				elif int(tmp_line[1]) == -2:
-					pdict2[int(tmp_line[2])] = int(tmp_line[1])
+					pdict2[int(tmp_line[2])] = -2
 
-		get_seq(pdict1, fq1, fPAIR1, fSE)
-		pdict1.clear()
-		get_seq(pdict2, fq2, fPAIR2, fSE)
-		pdict2.clear()
+		sys.stdout.write("[FIXPE] Pushing to files ...\n")
+		fOFQ1 = open(self.outp + "_r1.preqc.fastq", 'w')
+		fOFQ2 = open(self.outp + "_r2.preqc.fastq", 'w')
+		fORPHAN1 = open(self.outp + "_orphan.preqc.1.fastq", 'w')
+		fORPHAN2 = open(self.outp + "_orphan.preqc.2.fastq", 'w')
+		pid = os.fork()
+		if pid > 0:
+			sys.stdout.write("[FIXPE] Pushing to %s ...\n" %(self.outp+"_r2.preqc.fastq"))
+			self._get_seq(pdict2, self.fq2, fOFQ2, fORPHAN2)
+			pdict2.clear()
+		else:
+			child = pid
+			sys.stdout.write("[FIXPE] Pushing to %s ...\n" %(self.outp+"_r1.preqc.fastq"))
+			self._get_seq(pdict1, self.fq1, fOFQ1, fORPHAN1)
+			pdict1.clear()
+			os._exit(0)
+		fOFQ1.close()
+		fOFQ2.close()
+		fORPHAN1.close()
+		fORPHAN2.close()
 
-		fPAIR1.close()
-		fPAIR2.close()
-		fSE.close()
+		sys.stdout.write("[FIXPE] Concatenating orphan preqc fastq files ...\n")
+		cat_cmd = "cat %s %s " %(self.outp+"_orphan.preqc.1.fastq", self.outp+"_orphan.preqc.2.fastq")
+		pcat = subprocess.Popen(shlex.split(cat_cmd), stdout = open(self.outp + "_orphan.preqc.fastq", 'w'))
+		pcat_out, pcat_err = pcat.communicate()
+		if pcat.returncode != 0:
+			sys.stderr.write("[FIXPE] Fail to concatenate 2 orphan fastq files\n")
+			sys.exit(1)
 
-#if __name__ == "__main__":
-#	parser = ArgumentParser(description="Checking pair-end reads, only work for Casava 1.8+ style header")
-#	parser.add_argument("-fq1", metavar="FILE", dest="fq1", required=True, help="First-end fastq file. Required")
-#	parser.add_argument("-fq2", metavar="FILE", dest="fq2", required=True, help="Second-end fastq file. Required")
-#	parser.add_argument("-p", metavar="STR", dest = "outprefix", required=True, help="output prefix")
-#	parser.add_argument("-interleaved", action="store_true", dest="interleaved", help="toggle on to output interleaved fastq file. Optional.")
-#
-#	args = parser.parse_args()
-#
-#	check_inputs(args.fq1, args.fq2)
-#
-#	fix_pairing(args.fq1, args.fq2, args.outprefix, args.interleaved)
+		sys.stdout.write("[FIXPE] Cleaning ...\n")
+		os.remove(self.outp+"_orphan.preqc.1.fastq")
+		os.remove(self.outp+"_orphan.preqc.2.fastq")
